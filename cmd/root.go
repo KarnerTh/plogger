9c7f3a1f7990ec2dfd3c1c1fc796a5d6683db5d8
Thomas Karner <thomas.7.karner@gmail.com>
2023-12-12 19:15:51 +0100

    cleanup

--- cmd/root.go
+++ cmd/root.go
@@ -12,14 +12,9 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "plogger",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "plogger -t=<extractor-key>",
+	Short: "Plot your logger output",
+	Long:  `Plot your logger output`,
 	Run: func(cmd *cobra.Command, args []string) {
 		extractorType, err := cmd.Flags().GetString("type")
 		if err != nil {
@@ -44,14 +39,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.plogger.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().StringP("type", "t", "", "Log extractor type")
 }
 
--- extract/extractor.go
+++ extract/extractor.go
@@ -6,6 +6,7 @@ type Data struct {
 	LogLine string
 	Value   float64
 }
+
 type Extractor interface {
 	Key() string
 	Extract(logLine string) (Data, error)
